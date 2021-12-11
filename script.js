46a08de5856cac879a392f1a86802a0c21721a1a
Subash Khatri <subashkhatri44@gmail.com>
2021-12-11 12:59:27 -0600

    hrabbed data to DOM

--- script.js
+++ script.js
@@ -1,4 +1,32 @@
 $(document).ready(onReady);
 function onReady() {
     console.log('on ready');
+
+    $('#addEmployee').on('submit', onAddEmployee);
+}
+function onAddEmployee(event) {
+
+    // Preventing page from reload
+
+    event.preventDefault();
+
+    console.log('AnnualSalary added');
+
+    // NEED TO Grab  data from the DOM
+    //from my form inputs
+
+    let FirstName = $('#nameInput').val();
+    let LastName = $('#LastNameInput').val();
+    let ID = Number($('#NumberInput').val());
+    let Title = $('#TitleInput').val();
+    let AnnualSalary = Number($('#SalaryInput').val());
+
+    let employee = {
+        FirstName: FirstName,
+        LastName: LastName,
+        ID: ID,
+        Title: Title,
+        AnnualSalary: AnnualSalary
+    };
+    console.log('New employee', employee);
 }
