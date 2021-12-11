2b1a28dfb9f365abaaecd3c0403e0be7948eda14
Subash Khatri <subashkhatri44@gmail.com>
2021-12-11 16:59:26 -0600

    I think this is it

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 $(document).ready(onReady);
+
+let Newemployee = [];
+
 function onReady() {
     console.log('on ready');
 
     $('#addEmployee').on('submit', onAddEmployee);
+
+    //Handle delete product button
+    //$('.deleteBtn').on('click, onDeleteName');
+
+    $(document).on('click', '.deleteBtn', onDeleteName);
+    function onDeleteName() {
+     console.log('onDeleteName');
+
+     $(this).parents('tr').remove();
+
+
+
+    }
 }
 function onAddEmployee(event) {
 
@@ -29,4 +45,30 @@ function onAddEmployee(event) {
         AnnualSalary: AnnualSalary
     };
     console.log('New employee', employee);
+
+    //Adding Employee
+
+    Newemployee.push(employee);
+     //Empty <tbody>
+     $('#nameList').empty();
+
+    for (let name of  Newemployee){
+        $('#nameList').append(`
+        <tr>
+                <td>${employee.FirstName}</td>
+                <td>${employee.LastName}</td>
+                <td>${employee.ID}</td>
+                <td>${employee.Title}</td>
+                <td>$${employee.AnnualSalary}</td>
+                <td>
+                    <button class="deleteBtn">
+                        Delete
+                    </button>
+                </td>
+            </tr>
+        `);
+    }
+     
+    
+    
 }
